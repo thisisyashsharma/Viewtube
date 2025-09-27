142decc03fe028219c1b3c930efb6c2c150777ed
Yash Sharma <yashkarma7410@gmail.com>
2025-09-27 11:24:33 +0530

    EU6u3.p1.a1 & p2.a1 & p3.a1/a2 & p4.a1/a2/a3 - Subscribe feature - improved and working

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,89 +1,85 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-
 const userSignUp = new Schema(
-    {
-        name:{
-            type:String,
-            requiered:true,
-            unique: true,
-            trim: true,
-            index: true
-        },
-        email:{
-            type:String,
-            requiered:true,
-            unique: true,
-            lowercase: true,
-            trim: true 
-        },
-        password:{
-            type:String,
-            requiered:true
-        },
-        avatar:{
-            type:String
-        },
-        watchHistory:[
-            {
-                type : Schema.Types.ObjectId,
-                ref: "Video"         //3. Error - Token error -Step1 - fixed from "video" to "Video"
-            }
-        ],
-        refreshToken:{
-            type: String
-        },
-
-
+  {
+    name: {
+      type: String,
+      requiered: true,
+      unique: true,
+      trim: true,
+      index: true,
     },
-    {
-        timestamps:true
-    }
-)
-
+    email: {
+      type: String,
+      requiered: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      requiered: true,
+    },
+    avatar: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video", //3. Error - Token error -Step1 - fixed from "video" to "Video"
+      },
+    ],
+    refreshToken: {
+      type: String,
+    },
+    //EU6u3.p1.a1.2ln - Subscribe feature: +2 
+    subscribers: [{ type: Schema.Types.ObjectId, ref: "newUser" }],
+    subscribedTo: [{ type: Schema.Types.ObjectId, ref: "newUser" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // hash if changed
-userSignUp.pre("save", async function(next){
-  if(!this.isModified("password")) return next();
+userSignUp.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSignUp.methods.isPasswordCorrect = async function (password) {
-
-    /*
+  /*
        3.ERROR - Token Error - step2 - return this.password === password;
     */
-    return bcrypt.compare(password, this.password);     
+  return bcrypt.compare(password, this.password);
 };
 
-userSignUp.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            name: this.name
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "7d"
-        }
-    )
-}
-userSignUp.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY ||"30d"
-        }
-    )
-}
+userSignUp.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "7d",
+    }
+  );
+};
+userSignUp.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "30d",
+    }
+  );
+};
 
-
-export const newUser = mongoose.model("newUser" , userSignUp)
+export const newUser = mongoose.model("newUser", userSignUp);
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { deleteAccount, registerUser ,  login , updateAccount , logoutUser , refreshAccessToken , getUserById , GetWatchHistory , addToWatchHistory} from "../controllers/account.controller.js";
+
+//EU6u3.p3.a1.2words - Subscribe feature - imported toggleSubscribe, getSubscribeStatus
+import { deleteAccount, registerUser ,  login , updateAccount , logoutUser , refreshAccessToken , getUserById , GetWatchHistory , addToWatchHistory, toggleSubscribe, getSubscribeStatus } from "../controllers/account.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -15,4 +17,8 @@ router.route("/userData/:id").get(getUserById)
 router.route("/history").get(verifyJWT , GetWatchHistory)
 router.route("/addToHistory/:id").put(verifyJWT , addToWatchHistory)
 
+
+//EU6u3.p3.a2.2ln - Subscribe feature - routed both features 
+router.put("/subscribe/:channelId", verifyJWT, toggleSubscribe)
+router.get("/subscribe/status/:channelId", verifyJWT, getSubscribeStatus)
 export default router
